bfa1d26934673b43124b8d0543177ab008cb72f5
unknown <hindhujhasridhar@gmail.com>
2022-01-16 22:04:46 +0530

    FREQUENCY OF WORDS IN A PARAGRAPH

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -17,6 +17,27 @@ namespace HashTable
             Console.WriteLine("5th index value is:" + hash5);
             string hash3 = hash.Get(3);
             Console.WriteLine("3rd index value is:" + hash3);
+            //PARAGRAPH
+            hash.Add(6, "PARANOIDS");
+            hash.Add(7, "ARE");
+            hash.Add(6, "NOT");
+            hash.Add(8, "PARANOID");
+            hash.Add(9, "BECAUSE");
+            hash.Add(10, "THEY");
+            hash.Add(11, "KEEP");
+            hash.Add(12, "PUTTING");
+            hash.Add(13, "THEMSELVES");
+            hash.Add(14, "DELIBERATELY");
+            hash.Add(15, "INTO");
+            hash.Add(16, "PARANOID");
+            hash.Add(17, "AVOIDABLE");
+            hash.Add(18, "SITUATIONS");
+            string hash10 = hash.Get(5);
+            Console.WriteLine("10th index value is:" + hash10);
+            string hash15 = hash.Get(3);
+            Console.WriteLine("15th index value is:" + hash15);
+
+
         }
     }
 }
